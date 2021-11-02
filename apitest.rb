e908e9541b7272e3992ecdced29ffde7bead6656
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2021-11-01 23:00:23 -0700

    initial

---
+++ apitest.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'sinatra/namespace'
+
+namespace '/api/v1' do
+  before do
+    content_type 'application/json'
+  end
+
+  get '/*' do
+    path = "no path"
+    vars = {}
+    status = 200
+    params.each do |key, val|
+      if key == 'splat'
+        path = val.first
+      elsif key == 'status'
+        status = val
+      else
+        vars[key] = val
+      end
+    end
+    params[:splat].first
+    {status: status, path: path, vars: vars}.to_json
+  end
+end
